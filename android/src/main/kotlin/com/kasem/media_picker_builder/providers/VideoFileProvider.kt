eeca13d6268a9fdb16bafb91ffcf6e512a646727
astyrc <adam.styrc@vattenfall.com>
2020-11-12 18:26:14 +0100

    Change Float to Double

--- android/src/main/kotlin/com/kasem/media_picker_builder/providers/VideoFileProvider.kt
+++ android/src/main/kotlin/com/kasem/media_picker_builder/providers/VideoFileProvider.kt
@@ -3,37 +3,61 @@ package com.kasem.media_picker_builder.providers
 import android.content.Context
 import android.database.Cursor
 import android.media.MediaMetadataRetriever
+import android.os.Build
 import android.provider.MediaStore
 import com.kasem.media_picker_builder.model.Album
 import com.kasem.media_picker_builder.model.MediaFile
 
 object VideoFileProvider {
 
-    private val VIDEO_MEDIA_COLUMNS = arrayOf(
-            MediaStore.Video.Media._ID,
-            MediaStore.Video.Media.DATE_ADDED,
-            MediaStore.Video.Media.DATA,
-            MediaStore.Video.Media.MIME_TYPE
-    )
-
     fun getVideoMediaFile(
             context: Context,
             fileId: Long
     ): MediaFile? {
-        val cursor = context.contentResolver.query(
-                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                VIDEO_MEDIA_COLUMNS,
-                "${MediaStore.Video.Media._ID} = $fileId",
-                null,
-                null)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val cursor = context.contentResolver.query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(
+                            MediaStore.Video.Media._ID,
+                            MediaStore.Video.Media.DATE_ADDED,
+                            MediaStore.Video.Media.DATA,
+                            MediaStore.Video.Media.MIME_TYPE,
+                            MediaStore.Video.Media.BUCKET_ID,
+                            MediaStore.Video.Media.DURATION,
+                            MediaStore.Video.Media.ORIENTATION),
+                    "${MediaStore.Video.Media._ID} = $fileId",
+                    null,
+                    null)
 
-        val mediaMetadataRetriever = MediaMetadataRetriever()
-        if (cursor != null && cursor.moveToFirst()) {
-            return cursorToMediaFile(mediaMetadataRetriever, cursor)
+            return if (cursor != null && cursor.moveToFirst()) {
+                cursorToMediaFileSubsequent29(cursor)
+            } else {
+                null
+            }
+        } else {
+            val cursor = context.contentResolver.query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(
+                            MediaStore.Video.Media._ID,
+                            MediaStore.Video.Media.DATE_ADDED,
+                            MediaStore.Video.Media.DATA,
+                            MediaStore.Video.Media.MIME_TYPE),
+                    "${MediaStore.Video.Media._ID} = $fileId",
+                    null,
+                    null)
+
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+
+            val mediaFile =
+                    if (cursor != null && cursor.moveToFirst()) {
+                        cursorToMediaFilePriorTo29(mediaMetadataRetriever, cursor)
+                    } else {
+                        null
+                    }
+            mediaMetadataRetriever.release()
+
+            return mediaFile
         }
-        mediaMetadataRetriever.release()
-
-        return null
     }
 
     fun fetchVideos(
@@ -50,35 +74,95 @@ object VideoFileProvider {
             selectionArgs = arrayOf(startDate.toString(), endDate.toString())
         }
 
-        val cursor = context.contentResolver.query(
-                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                VIDEO_MEDIA_COLUMNS,
-                selectionClause,
-                selectionArgs,
-                "${MediaStore.Video.Media._ID} DESC")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val cursor = context.contentResolver.query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(
+                            MediaStore.Video.Media._ID,
+                            MediaStore.Video.Media.DATE_ADDED,
+                            MediaStore.Video.Media.DATA,
+                            MediaStore.Video.Media.MIME_TYPE,
+                            MediaStore.Video.Media.BUCKET_ID,
+                            MediaStore.Video.Media.DURATION,
+                            MediaStore.Video.Media.ORIENTATION),
+                    selectionClause,
+                    selectionArgs,
+                    "${MediaStore.Video.Media._ID} DESC")
 
-        val mediaMetadataRetriever = MediaMetadataRetriever()
-        while (cursor?.moveToNext() == true) {
-            val mediaFile = cursorToMediaFile(mediaMetadataRetriever, cursor)
+            while (cursor?.moveToNext() == true) {
+                val mediaFile = cursorToMediaFileSubsequent29(cursor)
 
-            if (mediaFile != null) {
-                val album = albumHashMap[mediaFile.albumId]
-                if (album == null) {
-                    albumHashMap[mediaFile.albumId] = Album(
-                            mediaFile.albumId,
-                            mediaFile.albumName,
-                            mutableListOf(mediaFile)
-                    )
-                } else {
-                    album.files.add(mediaFile)
+                if (mediaFile != null) {
+                    val album = albumHashMap[mediaFile.albumId]
+                    if (album == null) {
+                        albumHashMap[mediaFile.albumId] = Album(
+                                mediaFile.albumId,
+                                mediaFile.albumName,
+                                mutableListOf(mediaFile)
+                        )
+                    } else {
+                        album.files.add(mediaFile)
+                    }
                 }
             }
+        } else {
+            val cursor = context.contentResolver.query(
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(
+                            MediaStore.Video.Media._ID,
+                            MediaStore.Video.Media.DATE_ADDED,
+                            MediaStore.Video.Media.DATA,
+                            MediaStore.Video.Media.MIME_TYPE),
+                    selectionClause,
+                    selectionArgs,
+                    "${MediaStore.Video.Media._ID} DESC")
+
+            val mediaMetadataRetriever = MediaMetadataRetriever()
+            while (cursor?.moveToNext() == true) {
+                val mediaFile = cursorToMediaFilePriorTo29(mediaMetadataRetriever, cursor)
+
+                if (mediaFile != null) {
+                    val album = albumHashMap[mediaFile.albumId]
+                    if (album == null) {
+                        albumHashMap[mediaFile.albumId] = Album(
+                                mediaFile.albumId,
+                                mediaFile.albumName,
+                                mutableListOf(mediaFile)
+                        )
+                    } else {
+                        album.files.add(mediaFile)
+                    }
+                }
+            }
+            mediaMetadataRetriever.release()
         }
-        mediaMetadataRetriever.release()
     }
 
 
-    private fun cursorToMediaFile(
+    private fun cursorToMediaFileSubsequent29(cursor: Cursor): MediaFile? {
+        val fileId = cursor.getLong(0)
+        val fileDateAdded = cursor.getLong(1)
+        val filePath = cursor.getString(2)
+        val mimeType = cursor.getString(3)
+        val albumId = cursor.getLong(4)
+        val duration = cursor.getDouble(5) / 1000
+        val orientation = cursor.getInt(6)
+
+        return MediaFile(
+                fileId,
+                albumId,
+                "",
+                fileDateAdded,
+                filePath,
+                null,
+                orientation,
+                mimeType,
+                duration,
+                MediaFile.MediaType.VIDEO
+        )
+    }
+
+    private fun cursorToMediaFilePriorTo29(
             mediaMetadataRetriever: MediaMetadataRetriever,
             cursor: Cursor
     ): MediaFile? {
@@ -100,7 +184,7 @@ object VideoFileProvider {
         return MediaFile(
                 fileId,
                 0,
-                "", //Passing empty string, because real `albumName` was problematic under certain circumstances
+                "",
                 fileDateAdded,
                 filePath,
                 null,
